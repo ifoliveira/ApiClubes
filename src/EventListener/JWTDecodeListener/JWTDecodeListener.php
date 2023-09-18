48ec1f6e4de4630d998bbac20bba3f91a1470c30
ifoliveira <ifoliveira@hotmail.com>
2023-09-18 12:50:22 +0200

    Update JWTDecodeListener.php

---
+++ src/EventListener/JWTDecodeListener/JWTDecodeListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\EventListener\JWTDecodeListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodeEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+
+
+class JWTDecodeListener
+{
+    private $requestStack;
+   
+}
